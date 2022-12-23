04f1d71d63933c5df6208899cb2fc8c1159546cf
Rob Vanarsdall <rvanar@gmail.com>
2022-12-22 16:43:59 -0800

    added strict to remove the message in console

--- app.js
+++ app.js
@@ -2,11 +2,10 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const userController = require("./controllers/user.controller");
-// console.log(process.env);
-
+const movieController = require("./controllers/movie.controller");
 // ! Connecting to the DB
 const mongoose = require("mongoose");
-
+mongoose.set("strictQuery", false);
 mongoose.connect("mongodb://localhost:27017/moviedb");
 const db = mongoose.connection;
 
@@ -15,6 +14,7 @@ db.once("open", () => console.log("Connected to the DB"));
 app.use(express.json());
 
 app.use("/user", userController);
+app.use("/movie", movieController);
 app.listen(process.env.PORT, function () {
   console.log(`movie app is listening on port ${process.env.PORT}`);
 });
